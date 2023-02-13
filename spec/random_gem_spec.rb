cc4fe023e929a666afe18424d48b1b8479a4f77d
a5-stable <sh07e1916@gmail.com>
2023-02-13 23:39:11 +0900

    add #pick spec

--- spec/random_gem_spec.rb
+++ spec/random_gem_spec.rb
@@ -1,9 +1,25 @@
+require 'net/http'
+
 RSpec.describe RandomGem do
+  let(:response) { [{"documentation_uri"=>"sample_uri", "name"=>"sample_name"}] }
+
+  before do
+    double = instance_double(RandomGem::Request)
+    allow(RandomGem::Request).to receive(:new).and_return(double)
+    allow(double).to receive(:do).and_return(response)
+  end
+
   it "has a version number" do
     expect(RandomGem::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe "#pick" do
+    it "returns random gem response" do
+      gem = RandomGem.pick
+
+      expect( gem.class ).to eq Hash
+      expect( gem["documentation_uri"] ).to eq "sample_uri"
+      expect( gem["name"] ).to eq "sample_name"
+    end
   end
 end
